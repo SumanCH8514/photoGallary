79ac9e01b08a49219855cf089818f0aae1cd1cbd
Suman Chakrabortty <suman.chakrabortty8514@gmail.com>
2025-02-20 04:29:25 +0530

    fav fixed

--- new-version/admin/login.php
+++ new-version/admin/login.php
@@ -54,6 +54,23 @@ $conn->close();
     <link rel="stylesheet" href="../css/login.css">
     <!-- Font Awesome for Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="apple-touch-icon" sizes="57x57" href="../fav/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="../fav/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="../fav/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="../fav/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="../fav/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="../fav/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="../fav/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="../fav/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="../fav/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="../fav/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../fav/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="../fav/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../fav/favicon-16x16.png">
+    <link rel="manifest" href="../fav/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="../fav/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
 </head>
 
 <body>
@@ -74,6 +91,25 @@ $conn->close();
             <button type="submit" class="login-button">Login</button>
         </form>
         <p>Don't have an account? <a href="register.php">Register here</a></p>
+        <p>&copy; 2018-2025 <a href="../index.php" style="text-decoration: none;"><b>SumanPhotoGallery</b></a></p>
+    </div>
+    <div id="footer">
+        <p style="font-size: 0.7rem; text-align: center">
+            &copy; 2018-<span id="copyright"></span>
+            <span id="copyright">
+                <script>
+                    document
+                        .getElementById("copyright")
+                        .appendChild(document.createTextNode(new Date().getFullYear()));
+                </script>
+            </span>
+            <a
+                href="https://sumanonline.com "
+                target="_blank"
+                style="text-decoration: none; font-weight: bold">SumanOnline.com</a>
+            | All Rights Reserved.
+        </p>
+
     </div>
 </body>
 
--- new-version/admin/register.php
+++ new-version/admin/register.php
@@ -59,6 +59,23 @@ $conn->close();
     <link rel="stylesheet" href="../css/reg.css">
     <!-- Font Awesome for Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="apple-touch-icon" sizes="57x57" href="../fav/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="../fav/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="../fav/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="../fav/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="../fav/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="../fav/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="../fav/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="../fav/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="../fav/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="../fav/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../fav/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="../fav/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../fav/favicon-16x16.png">
+    <link rel="manifest" href="../fav/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="../fav/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
 </head>
 
 <body>
@@ -82,6 +99,26 @@ $conn->close();
             <button type="submit" class="register-button">Register</button>
         </form>
         <p>Already have an account? <a href="login.php">Login here</a></p>
+        <br>
+        <p>&copy; 2018-2025 <a href="../index.php" style="text-decoration: none;"><b>SumanPhotoGallery</b></a></p>
+    </div>
+    <div id="footer">
+        <p style="font-size: 0.7rem; text-align: center">
+            &copy; 2018-<span id="copyright"></span>
+            <span id="copyright">
+                <script>
+                    document
+                        .getElementById("copyright")
+                        .appendChild(document.createTextNode(new Date().getFullYear()));
+                </script>
+            </span>
+            <a
+                href="https://sumanonline.com "
+                target="_blank"
+                style="text-decoration: none; font-weight: bold">SumanOnline.com</a>
+            | All Rights Reserved.
+        </p>
+
     </div>
 </body>
 
